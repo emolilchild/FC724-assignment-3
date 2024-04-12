3ac933bac3b727f7b1323e8b12f530889557e6aa
emolilchild <andreqtqng@gmail.com>
2024-04-12 20:55:47 +0100

    Create task 2.py

---
+++ task 2.py
@@ -0,0 +1,12 @@
+#convert my psuedocode into python syntax
+def euclideam_algorithm(num1, num2):
+    # repeatedly loop until num 2 becomes 0 so that num 1 can hold the GCD
+    while num2 != 0:
+        # store the value of num2 into a temporary variable
+        temp = num2
+        # redefine num2 as the remainder of num1/num2
+        num2 = num1 % num2
+        # redefine num1 as the previous value of num2 using the temp variable
+        num1 = temp
+    # when the iteration is done num1 will be the GCD
+    return num1
